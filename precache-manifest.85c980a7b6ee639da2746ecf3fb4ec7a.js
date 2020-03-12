c1b73075a9e04995ef06b72c1f256c2329f8a012
PRETHIV <46440591+PRETHIV@users.noreply.github.com>
2020-03-12 23:23:21 +0530

    Updates

---
+++ precache-manifest.85c980a7b6ee639da2746ecf3fb4ec7a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e50ef122d377cdd1a14fbfb088d728b",
+    "url": "/tictacreact/index.html"
+  },
+  {
+    "revision": "9e65de9c24b2d015673b",
+    "url": "/tictacreact/static/css/2.f65acde2.chunk.css"
+  },
+  {
+    "revision": "ab4263e9b31844ca540a",
+    "url": "/tictacreact/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9e65de9c24b2d015673b",
+    "url": "/tictacreact/static/js/2.af8f5d68.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/tictacreact/static/js/2.af8f5d68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab4263e9b31844ca540a",
+    "url": "/tictacreact/static/js/main.8f7a2b9d.chunk.js"
+  },
+  {
+    "revision": "b583697f4b875e5b86fd",
+    "url": "/tictacreact/static/js/runtime-main.f64dddea.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tictacreact/static/media/logo.5d5d9eef.svg"
+  }
+]);
